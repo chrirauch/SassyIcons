cce0a6bdee04d453d2c1b361a6a8ff10a6bad65b
Pascal Duez <pascal@duez.me>
2014-09-12 19:33:04 +0200

    Update gh-pages

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -6,34 +6,15 @@
   // Constructor
   var App = function (conf) {
     this.conf = $.extend({
-      // Search DOM
-      search: {
-        items: '.sassdoc__item',
-        input: '#js-search-input',
-        form: '#js-search',
-        suggestionsWrapper: '#js-search-suggestions'
-      },
+      // Search module
+      search: new global.Search(),
 
-      // Fuse options
-      fuse: {
-        keys: ['name'],
-        threshold: 0.3
-      },
+      // Sidebar module
+      sidebar: new global.Sidebar(),
 
       // Initialisation
       init: true
     }, conf || {});
-
-    // Fuse engine instanciation
-    this.index = new Fuse($.map($(this.conf.search.items), function (item) {
-      var $item = $(item);
-
-      return {
-        name: $item.data('name'),
-        type: $item.data('type'),
-        node: $item
-      };
-    }), this.conf.fuse);
 
     // Launch the module
     if (this.conf.init !== false) {
@@ -43,125 +24,23 @@
 
   // Initialisation method
   App.prototype.initialize = function () {
-    this.initializeSearch();
     this.codePreview();
-    this.foldingSidebar();
-  };
-
-  // Fill DOM with search suggestions
-  App.prototype.fillSuggestions = function (items) {
-    var searchSuggestions = $(this.conf.search.suggestionsWrapper);
-    searchSuggestions.html('');
-
-    var suggestions = $.map(items.slice(0, 10), function (item) {
-      var $li = $('<li />', {
-        'data-type': item.type,
-        'data-name': item.name,
-        'html': '<a href="#' + item.type + '-' + item.name + '"><code>' + item.type.slice(0, 3) + '</code> ' + item.name + '</a>'
-      });
-
-      searchSuggestions.append($li);
-      return $li;
-    });
-
-    return suggestions;
-  };
-
-  // Perform a search on a given term
-  App.prototype.search = function (term) {
-    return this.fillSuggestions(this.index.search(term));
-  };
-
-  // Search logic
-  App.prototype.initializeSearch = function () {
-    var searchForm = $(this.conf.search.form);
-    var searchInput = $(this.conf.search.input);
-    var searchSuggestions = $(this.conf.search.suggestionsWrapper);
-
-    var currentSelection = -1;
-    var suggestions = [];
-    var selected;
-
-    var self = this;
-
-    // Clicking on a suggestion
-    searchSuggestions.on('click', function (e) {
-      var target = $(event.target);
-
-      if (target.nodeName === 'A') {
-        searchInput.val(target.parent().data('name'));
-        suggestions = self.fillSuggestions([]);
-      }
-    });
-
-    // Filling the form
-    searchForm.on('keyup', function (e) {
-      e.preventDefault();
-
-      // Enter
-      if (e.keyCode === 13) {
-        if (selected) {
-          suggestions = self.fillSuggestions([]);
-          searchInput.val(selected.data('name'));
-          window.location = selected.children().first().attr('href');
-        }
-
-        e.stopPropagation();
-      }
-
-      // KeyDown
-      if (e.keyCode === 40) {
-        currentSelection = (currentSelection + 1) % suggestions.length;
-      }
-
-      // KeyUp
-      if (e.keyCode === 38) {
-        currentSelection = currentSelection - 1;
-
-        if (currentSelection < 0) {
-          currentSelection =  suggestions.length - 1;
-        }
-      }
-
-      if (suggestions[currentSelection]) {
-        if (selected) {
-          selected.removeClass('selected');
-        }
-
-        selected = suggestions[currentSelection];
-        selected.addClass('selected');
-      }
-
-    });
-
-    searchInput.on('keyup', function (e) {
-      if (e.keyCode !== 40 && e.keyCode !== 38) {
-        currentSelection = -1;
-        suggestions = self.search($(this).val());
-      }
-
-      else {
-        e.preventDefault();
-      }
-    }).on('search', function () {
-      suggestions = self.search($(this).val());
-    });
   };
 
   // Toggle code preview collapsed/expanded modes
   App.prototype.codePreview = function () {
-    $('.item__code').on('click', function () {
-      $(this).parent().toggleClass('code--collapsed code--expanded');
-    });
-  };
+    var $item;
+    var $code;
+    var switchTo;
 
-  App.prototype.foldingSidebar = function () {
-    $('.sidebar__item--heading').on('click', function () {
-      $(this).nextUntil('.sidebar__item--heading', 'ul, .sidebar__item--sub-heading').toggleClass('collapsed');
-    });
+    $('.item__code--togglable').on('click', function () {
+      $item = $(this);
+      $code = $item.find('code');
+      switchTo = $item.attr('data-current-state') === 'expanded' ? 'collapsed' : 'expanded';
 
-    $('.sidebar__item--sub-heading').on('click', function () {
-      $(this).next('ul').toggleClass('collapsed');
+      $item.attr('data-current-state', switchTo);
+      $code.html($item.attr('data-' + switchTo));
+      Prism.highlightElement($code[0]);
     });
   };
 
